0b65d0cb10c4356d85710bc69b90eeb6605fd72d
Munna Hossain <mayinuddin.munna@gmail.com>
2021-02-23 22:21:15 +0600

    different id data created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 
-
-function Person() {
-    return (
-        <div style={{ border: '2px solid red', margin: '10px', width: '35%', borderRadius: '5px', backgroundColor: 'gray' }}>
-            <h2>Web Developer</h2>
-            <h4>Hello world!</h4>
-        </div>
-    )
-}
 function App() {
     var person = {
         name: 'Abdullah',
         age: 67,
         type: 'AnyPeople'
     }
-    var personTwo = {
-        name: 'Mohammad',
-        age: 67,
-        type: 'Prophet'
-    }
+    // var personTwo = {
+    //     name: 'Mohammad',
+    //     age: 67,
+    //     type: 'Prophet'
+    // }
     var style = {
         color: 'red',
         backgroundColor: 'gray',
@@ -28,17 +19,41 @@ function App() {
         padding: '5px',
         margin: '3px'
     }
+
+    const developer=['Munna','sajol','chisty'];
     return (
         <div className="App">
             <header className="App-header">
                 <h2>Sum of numbers : {(2 + 8)}</h2>
                 <h3 style={style} >Identity: {person.type + " " + person.name}</h3>
-                <h3 style={style} >Identity: {personTwo.type + " " + personTwo.name}</h3>
-                <Person></Person>
-                <Person></Person>
-                <Person></Person>
+                {/* <h3 style={style} >Identity: {personTwo.type + " " + personTwo.name}</h3> */}
+                <Person devPerson="Munna Hossain" homeTown="Chandpur"></Person>
+                <Person devPerson="chisty" homeTown="MAdaripur"></Person>
+                <Person devPerson="sajol" homeTown="Comilla"></Person>
+
+                {/* Dynamic call */}
+                <h1>Dynamic called</h1>
+                <Person devPerson={developer[0]} homeTown="Madaripur"></Person>
+                <Person devPerson={developer[1]} homeTown="Madaripur"></Person>
+                <Person devPerson={developer[2]} homeTown="Brammonbariya"></Person>
             </header>
         </div>
     );
+}
+function Person(props) {
+    var cardStyle = {
+        border: '2px solid red',
+        margin: '10px',
+        width: '35%',
+        borderRadius: '5px',
+        backgroundColor: 'gray'
+    }
+    return (
+        <div style={cardStyle}>
+            <h2>Web Developer</h2>
+            <h4>Name: {props.devPerson}</h4>
+            <h5>Village: {props.homeTown}</h5>
+        </div>
+    )
 }
 export default App;
